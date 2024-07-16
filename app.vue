85047fc93aa71f9c6369807a643ed700f20407c4
Adnan Sahin <adnansahin1@outlook.com>
2024-07-16 13:38:49 +0300

    * Created a new nuxt Project * Created TrelloBoard and TrelloBoardTask components * Installed and configured tailwindcss library

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="mb-2 items-center flex">
+      <img src="./assets/images/task-logo.png" alt="logo" width="120px" />
+    </h1>
+    <TrelloBoard />
+  </div>
+</template>
